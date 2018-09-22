6fbe30c1fa1639f6bcb193ac8af12f48e3f29e32
AlbericoD <a.diasbarreto@icloud.com>
2018-09-22 00:31:05 -0300

    testing scanner 1

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const {UsbScanner , getDevices} = require('usb-barcode-scanner');
 // console.log("PoobInfo: \nStart Discovery All Devices ... \n", getDevices());
  console.log("PoobInfo Log: \n")
 let scanner = new UsbScanner({
-    vendorId: 32903,
-    productId: 2590
+    vendorId: 1626,
+    productId: 40961
 });
  
 scanner.on('data', (data) => {
